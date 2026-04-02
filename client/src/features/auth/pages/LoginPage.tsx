eb8c649eef5c0406002f1923d2f1c4ba3e19b70a
Ron <lucifron28@gmail.com>
2026-04-02 14:01:44 +0800

    refactor: simplify LoginPage component by removing unnecessary markup

--- client/src/features/auth/pages/LoginPage.tsx
+++ client/src/features/auth/pages/LoginPage.tsx
@@ -1,9 +1,5 @@
 import { LoginForm } from '@/features/auth/components/LoginForm'
 
 export function LoginPage() {
-  return (
-    <main className="mx-auto flex min-h-screen w-full max-w-5xl items-center justify-center p-6">
-      <LoginForm />
-    </main>
-  )
+  return <LoginForm />
 }
