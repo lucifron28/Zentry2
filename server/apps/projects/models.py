897f549887e25ad7a835e70ffa20eb6fd8363dec
Ron <lucifron28@gmail.com>
2026-04-05 17:57:40 +0800

    feat: prevent project owner removal from members via m2m_changed signal

--- server/apps/projects/models.py
+++ server/apps/projects/models.py
@@ -2,6 +2,9 @@
 
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from django.core.exceptions import ValidationError
 
 from apps.core.models.base import TimeStampedModel
 
@@ -63,3 +66,13 @@ class Project(TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+@receiver(m2m_changed, sender=Project.members.through)
+def project_members_changed(sender, instance, action, pk_set, **kwargs):
+    """Enforce that the owner must remain a member of the project."""
+    if action == 'pre_remove':
+        if instance.owner_id in pk_set:
+            raise ValidationError('Project owner cannot be removed from members.')
+    elif action == 'pre_clear':
+        if instance.members.filter(pk=instance.owner_id).exists():
+            raise ValidationError('Project owner cannot be removed from members.')
